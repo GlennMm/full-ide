d144a1b5e7fc7e85bcb1114c7707a2837023b50a
GlennMm <matsiweglenn@gmail.com>
2023-08-27 13:47:36 +0200

    bakcup

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -5,11 +5,8 @@ return {
     return {
       root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
       sources = {
-        nls.builtins.formatting.fish_indent,
-        nls.builtins.diagnostics.fish,
         nls.builtins.formatting.stylua,
         nls.builtins.formatting.shfmt,
-        nls.builtins.diagnostics.flake8,
         nls.builtins.diagnostics.eslint, -- eslint or eslint_d
         nls.builtins.code_actions.eslint, -- eslint or eslint_d
         nls.builtins.formatting.prettier, -- prettier, eslint, eslint_d, or prettierd
